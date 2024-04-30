86072131d8ad625a8e7161b7d057c90a82bc9f6d
youht <youht@youhtdeMac-mini.local>
2024-04-30 19:31:00 +0800

    update stream

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,32 +1,24 @@
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
-import { Card } from '../../ui/dashboard/cards';
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import CardWrapper from '../../ui/dashboard/cards';
 import LatestInvoices from '../../ui/dashboard/latest-invoices';
 import RevenueChart from '../../ui/dashboard/revenue-chart';
  
 export default async function Page() {
   console.log("rendered !!!!!")
-  const revenue = await fetchRevenue(); 
-  const latestInvoices = await fetchLatestInvoices();
-  const {totalPaidInvoices,totalPendingInvoices,numberOfInvoices,numberOfCustomers} = await fetchCardData();
+ 
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {<Card title="Collected" value={totalPaidInvoices} type="collected" />}
-        {<Card title="Pending" value={totalPendingInvoices} type="pending" /> }
-        {<Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> }
-        {<Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> }
+        <Suspense fallback={<CardsSkeleton/>}>{<CardWrapper/>}</Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {<RevenueChart revenue={revenue}  /> }
-        {<LatestInvoices latestInvoices={latestInvoices} /> }
+        <Suspense fallback={<RevenueChartSkeleton/>}> {<RevenueChart /> } </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton/>}>{<LatestInvoices /> } </Suspense>
       </div>
     </main>
   );
